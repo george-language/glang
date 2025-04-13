cb3d14caf8ce93e7204149b0d87dcd7ee4a5679c
kelle <kellerhydle@gmail.com>
2025-04-13 14:48:54 -0600

    Language Updates
    -fixed small errors

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from src.language import run
 
 VERSION = '1.0.0'
 
-if getattr(sys, 'frozen', False):
-    os.chdir(sys._MEIPASS)
-
 file_path = None
 
 try:
@@ -16,17 +13,21 @@ except:
     pass
 
 if file_path:
-    if file_path.endswith('.glang') and os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            text = f.read()
+    if file_path.endswith('.glang'):
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                text = f.read()
 
-            result, error = run(file_path, text)
+                result, error = run(file_path, text)
 
-            if error:
-                print(error.asString())
+                if error:
+                    print(error.asString())
 
-            elif result:
-                print(f'George Debug: {repr(result)}')
+                elif result:
+                    print(f'George Debug: {repr(result)}')
+
+        else:
+            print(f'File "{file_path}" is not a valid file')
 
     else:
         print(f'Wrong File Type: "{file_path}" is not a ".glang" file.')
