942657daa3e059de3dc710ea3905b6727af1e111
kelle <kellerhydle@gmail.com>
2025-06-06 11:27:59 -0600

    Repo Updates
    -added more rust setup
    -tasks.json for running test files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+const VERSION: &str = "2.0";
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = env::args();
+
+    if arguments.len() > 0 {
+        for arg in arguments {
+            if arg.ends_with(".glang") {}
+        }
+    }
 }
