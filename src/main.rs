3eb3607c28f92412e71c93935f1dfc3763a8037f
kelle <kellerhydle@gmail.com>
2025-06-22 16:25:06 -0700

    Command Updates
    -create new/init projects

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use glang::run;
-use std::env;
 use std::io::{Write, stdin, stdout};
+use std::path::Path;
+use std::{env, fs};
 
 const VERSION: &str = "2.0";
 
@@ -11,12 +12,43 @@ fn main() {
         args.next(); // skip .exe
 
         if let Some(first_arg) = args.next() {
-            if first_arg.ends_with(".glang") {
+            if first_arg == "new" {
+                if let Some(second_arg) = args.next() {
+                    let dir_name = Path::new(second_arg.as_str());
+
+                    fs::create_dir(&dir_name).expect("Cannot create directory (invalid name)");
+                    fs::create_dir(&dir_name.join("src"));
+
+                    fs::write(
+                        &dir_name.join("main.glang"),
+                        "func main() {\n    bark(\"Hello world!\");\n}",
+                    );
+                    fs::write(
+                        &dir_name.join("README.md"),
+                        "# Welcome to GLang!\nTo get started, see our documentation [here](https://sites.google.com/view/george-lang/documentation).",
+                    );
+                }
+            } else if first_arg == "init" {
+                let dir_name = Path::new(".");
+
+                fs::create_dir(&dir_name.join("src"));
+
+                fs::write(
+                    &dir_name.join("main.glang"),
+                    "func main() {\n    bark(\"Hello world!\");\n}",
+                );
+                fs::write(
+                    &dir_name.join("README.md"),
+                    "# Welcome to GLang!\nTo get started, see our documentation [here](https://sites.google.com/view/george-lang/documentation).",
+                );
+            } else if first_arg.ends_with(".glang") {
                 let error = run(first_arg.as_str(), None);
 
                 if error.is_some() {
                     println!("{}", error.unwrap());
                 }
+            } else {
+                println!("Not a recognized GLang command")
             }
         }
     } else {
