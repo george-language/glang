0ad7ca5fadaac23a527e041b4b6eabc042bb02a3
kelle <kellerhydle@gmail.com>
2025-06-22 16:02:02 -0700

    Standard Library Updates
    -reorganized into individual modules

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,11 @@ use std::fs;
 
 pub fn run(filename: &str, code: Option<String>) -> Option<StandardError> {
     let mut contents = String::new();
-    contents.push_str(fs::read_to_string("modules/stdlib.glang").unwrap().as_str());
+    contents.push_str(
+        fs::read_to_string("modules/std/stdlib.glang")
+            .unwrap()
+            .as_str(),
+    );
 
     if filename == "<stdin>" {
         contents.push_str(code.unwrap_or_else(|| "".to_string()).as_str());
