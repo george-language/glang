0b57ea74b88d6e5a88cdd62283c28dc018fed794
kelle <kellerhydle@gmail.com>
2025-06-22 14:33:20 -0600

    Built Ins Updates
    -added dig/bury built ins for reading and writing files
    -updated fetch built in to verify circular imports

--- src/interpreting/interpreter.rs
+++ src/interpreting/interpreter.rs
@@ -43,6 +43,20 @@ impl Interpreter {
             )))),
         );
         interpreter.global_symbol_table.borrow_mut().set(
+            "dig".to_string(),
+            Some(Box::new(Value::BuiltInFunction(BuiltInFunction::new(
+                "dig",
+                interpreter.global_symbol_table.clone(),
+            )))),
+        );
+        interpreter.global_symbol_table.borrow_mut().set(
+            "bury".to_string(),
+            Some(Box::new(Value::BuiltInFunction(BuiltInFunction::new(
+                "bury",
+                interpreter.global_symbol_table.clone(),
+            )))),
+        );
+        interpreter.global_symbol_table.borrow_mut().set(
             "tostring".to_string(),
             Some(Box::new(Value::BuiltInFunction(BuiltInFunction::new(
                 "tostring",
--- src/values/built_in_function.rs
+++ src/values/built_in_function.rs
@@ -8,8 +8,7 @@ use crate::{
     parsing::parser::Parser,
     values::{list::List, number::Number, string::StringObj, value::Value},
 };
-use std::io::{Write, stdin, stdout};
-use std::{cell::RefCell, fs, rc::Rc};
+use std::{cell::RefCell, fs, io::Write, io::stdin, io::stdout, rc::Rc};
 
 #[derive(Debug, Clone)]
 pub struct BuiltInFunction {
@@ -119,6 +118,8 @@ impl BuiltInFunction {
         match self.name.as_str() {
             "bark" => return self.execute_print(args, &mut exec_context),
             "chew" => return self.execute_input(args, &mut exec_context),
+            "dig" => return self.execute_read(args, &mut exec_context),
+            "bury" => return self.execute_write(args, &mut exec_context),
             "tostring" => return self.execute_tostring(args, &mut exec_context),
             "tonumber" => return self.execute_tonumber(args, &mut exec_context),
             "clear" => return self.execute_clear(args, &mut exec_context),
@@ -175,6 +176,108 @@ impl BuiltInFunction {
             .expect("did not enter a valid string");
 
         result.success(Some(StringObj::from(input.trim())))
+    }
+
+    pub fn execute_read(&self, args: &Vec<Box<Value>>, exec_ctx: &mut Context) -> RuntimeResult {
+        let mut result = RuntimeResult::new();
+        result.register(self.check_and_populate_args(&vec!["file".to_string()], args, exec_ctx));
+
+        if result.should_return() {
+            return result;
+        }
+
+        let file_arg = args[0].clone();
+
+        let filename = match file_arg.as_ref() {
+            Value::StringValue(string) => string.as_string(),
+            _ => {
+                return result.failure(Some(StandardError::new(
+                    "expected type string".to_string(),
+                    file_arg.position_start().unwrap().clone(),
+                    file_arg.position_end().unwrap().clone(),
+                    Some("add a filename to read like 'test.txt'".to_string()),
+                )));
+            }
+        };
+
+        if !fs::exists(&filename).is_ok() {
+            return result.failure(Some(StandardError::new(
+                "file doesn't exist".to_string(),
+                file_arg.position_start().unwrap().clone(),
+                file_arg.position_end().unwrap().clone(),
+                Some("add a filename to read like 'test.txt'".to_string()),
+            )));
+        }
+
+        let mut contents = String::new();
+
+        match fs::read_to_string(&filename) {
+            Ok(extra) => contents.push_str(&extra),
+            Err(_) => {
+                return result.failure(Some(StandardError::new(
+                    "file contents couldn't be read properly".to_string(),
+                    file_arg.position_start().unwrap().clone(),
+                    file_arg.position_end().unwrap().clone(),
+                    Some("add a UTF-8 encoded file you would like to read".to_string()),
+                )));
+            }
+        }
+
+        result.success(Some(StringObj::from(contents.as_str())))
+    }
+
+    pub fn execute_write(&self, args: &Vec<Box<Value>>, exec_ctx: &mut Context) -> RuntimeResult {
+        let mut result = RuntimeResult::new();
+        result.register(self.check_and_populate_args(
+            &vec!["file".to_string(), "contents".to_string()],
+            args,
+            exec_ctx,
+        ));
+
+        if result.should_return() {
+            return result;
+        }
+
+        let file_arg = args[0].clone();
+        let contents_arg = args[1].clone();
+
+        let filename = match file_arg.as_ref() {
+            Value::StringValue(string) => string.as_string(),
+            _ => {
+                return result.failure(Some(StandardError::new(
+                    "expected type string".to_string(),
+                    file_arg.position_start().unwrap().clone(),
+                    file_arg.position_end().unwrap().clone(),
+                    Some("add a filename to write to like 'test.txt'".to_string()),
+                )));
+            }
+        };
+
+        let contents = match contents_arg.as_ref() {
+            Value::StringValue(string) => string.as_string(),
+            _ => {
+                return result.failure(Some(StandardError::new(
+                    "expected type string".to_string(),
+                    file_arg.position_start().unwrap().clone(),
+                    file_arg.position_end().unwrap().clone(),
+                    Some("add the file contents to write into the file".to_string()),
+                )));
+            }
+        };
+
+        match fs::write(&filename, &contents) {
+            Ok(_) => {}
+            Err(_) => {
+                return result.failure(Some(StandardError::new(
+                    "file contents couldn't be written properly".to_string(),
+                    file_arg.position_start().unwrap().clone(),
+                    file_arg.position_end().unwrap().clone(),
+                    None,
+                )));
+            }
+        }
+
+        result.success(Some(Number::null_value()))
     }
 
     pub fn execute_tostring(
@@ -331,6 +434,15 @@ impl BuiltInFunction {
                 import.position_start().unwrap().clone(),
                 import.position_end().unwrap().clone(),
                 Some("add the '.glang' file you would like to import".to_string()),
+            )));
+        }
+
+        if &file_to_import == &import.position_start().unwrap().filename {
+            return result.failure(Some(StandardError::new(
+                "circular import".to_string(),
+                import.position_start().unwrap().clone(),
+                import.position_end().unwrap().clone(),
+                None,
             )));
         }
 
