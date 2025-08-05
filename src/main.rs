13d04bc6cf06d9d2c474aad7ce280e8122c99052
kelle <kellerhydle@gmail.com>
2025-08-05 13:38:50 -0600

    Repo Updates
    -bump version to 2.7

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::{Parser, Subcommand};
 use std::env;
 use std::path::Path;
 
-const VERSION: &str = "2.6";
+const VERSION: &str = "2.7";
 
 #[derive(Parser)]
 #[command(name = "glang", version = VERSION, about = "The George Programming Language")]
